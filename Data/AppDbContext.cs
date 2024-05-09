f46eb5045691c391d75e76bb37917f7b3bc16792
Phan Vu <anhvu2182004@gmail.com>
2024-05-09 09:46:11 +0700

    [ADD]: Prj

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SWP391_DEMO.Data {
+    public class AppDbContext : DbContext {
+
+        public AppDbContext(DbContextOptions options) : base(options) {
+        }
+
+        public AppDbContext() {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json").Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DBConnectionString"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
