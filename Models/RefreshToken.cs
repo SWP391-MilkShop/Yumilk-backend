c11d4552611f3eb9b14992565d8df24231caff8d
khanght <khanghuynhtan.work@gmail.com>
2024-05-10 18:33:15 +0700

    [Add-Model] RefreshToken

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SWP391_DEMO.Models
+{
+    public partial class RefreshToken
+    {
+        public int Id { get; set; }
+
+        public string? Token { get; set; }
+
+        public DateTime? Expires { get; set; }
+
+        public Guid? UserId { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
+
