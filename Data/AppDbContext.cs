dbfb78915105e4a181a8dcba0e818b82f6e67bc1
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-11 11:44:07 +0700

    [ADD] Add all DbSet

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,7 +17,24 @@ namespace SWP391_DEMO.Data {
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
         public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<CartDetail> CartDetails { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<OrderStatus> OrderStatuses { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Unit> Units { get; set; }
+        public virtual DbSet<ProductAnalytic> ProductAnalytics { get; set; }
+        public virtual DbSet<ProductAttribute> ProductAttributes { get; set; }
+        public virtual DbSet<ProductAttributeValue> ProductAttributeValues { get; set; }
+        public virtual DbSet<ProductImage> ProductImages { get; set; }
+        public virtual DbSet<Voucher> Vouchers { get; set; }
+        public virtual DbSet<UserVoucher> UserVouchers { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             if (optionsBuilder.IsConfigured)
