83ce524f71acf2691c563dc6821e9e3662095969
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-11 13:45:42 +0700

    [UPDATE] add get all user service

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -1,4 +1,6 @@
-﻿using SWP391_DEMO.Repository;
+﻿using SWP391_DEMO.Entities;
+using SWP391_DEMO.Model;
+using SWP391_DEMO.Repository;
 
 namespace SWP391_DEMO.Service
 {
@@ -6,12 +8,40 @@ namespace SWP391_DEMO.Service
     {
         List<UserModel> GetAllUser();
     }
-    public class UserService
+    public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
         public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
+        }
+        public List<UserModel> GetAllUser()
+        {
+            var users = _userRepository.GetAllUser();
+            var models = new List<UserModel>();
+            foreach (var user in users)
+            {
+                var model = ToUserModel(user);
+                models.Add(model);
+            }
+            return models;
+        }
+        /// <summary>
+        /// This method is used to convert User to UserModel
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private static UserModel ToUserModel(User user)
+        {
+            var model = new UserModel
+            {
+                Username = user.Username,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                VerificationToken = user.VerificationToken,
+                RoleId = user.RoleId
+            };
+            return model;
         }
     }
 }
