9f04fbe989d04273473ba55f27c5496c73e0b73f
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-12 11:34:18 +0700

    [UPDATE] Thêm status code cho 1 số common exceptions

--- Infrastructure/GlobalExceptionHandler.cs
+++ Infrastructure/GlobalExceptionHandler.cs
@@ -10,6 +10,7 @@ namespace SWP391_DEMO.Infrastructure
             CancellationToken cancellationToken
         )
         {
+            httpContext.Response.StatusCode = GetStatusCode(exception);
             //prevent showing the actual exception message to the client
             var problemDetails = new ProblemDetails
             {
@@ -17,7 +18,7 @@ namespace SWP391_DEMO.Infrastructure
                 Status = httpContext.Response.StatusCode,
                 Title = "An error occurred while processing your request",
                 Detail = GetHelpLink(exception),
-                Type = "Error"
+                Type = "Error" //cai nay nen de documentation link
             };
             await httpContext.Response.WriteAsJsonAsync(
                 problemDetails,
@@ -40,6 +41,22 @@ namespace SWP391_DEMO.Infrastructure
                 InvalidOperationException => "https://tools.ietf.org/html/rfc7231#section-6.5.10",
                 KeyNotFoundException => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
                 _ => "https://tools.ietf.org/html/rfc7231#section-6.6"
+            };
+        }
+        /// <summary>
+        /// Get status code based on the exception type
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        private static int GetStatusCode(Exception exception)
+        {
+            return exception switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,
+                TimeoutException => StatusCodes.Status408RequestTimeout,
+                InvalidOperationException => StatusCodes.Status500InternalServerError,
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
             };
         }
     }
