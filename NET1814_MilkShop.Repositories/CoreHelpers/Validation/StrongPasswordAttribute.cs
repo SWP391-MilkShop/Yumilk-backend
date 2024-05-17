b87ff45efab238f5f7f51f3af6c2afe1a15ac4a0
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-17 18:17:05 +0700

    [ADD] Tạo các model để xử lí, tạo attribute validate password

---
+++ NET1814_MilkShop.Repositories/CoreHelpers/Validation/StrongPasswordAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NET1814_MilkShop.Repositories.CoreHelpers.Validation
+{
+    internal class StrongPasswordAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var password = value as string;
+
+            if (password == null)
+            {
+                return new ValidationResult("Password is required.");
+            }
+
+            if (password.Length < 7 || password.Length > 12)
+            {
+                return new ValidationResult("Password must be 7-12 characters long.");
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                return new ValidationResult("Password must contain at least one uppercase letter.");
+            }
+
+            if (!password.Any(char.IsLower))
+            {
+                return new ValidationResult("Password must contain at least one lowercase letter.");
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                return new ValidationResult("Password must contain at least one digit.");
+            }
+
+            if (password.IndexOfAny("!@#$%^&*()".ToCharArray()) == -1)
+            {
+                return new ValidationResult("Password must contain at least one special character.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ NET1814_MilkShop.Repositories/Models/SignUpModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using NET1814_MilkShop.Repositories.CoreHelpers.Validation;
+
+namespace NET1814_MilkShop.Repositories.Models
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        [Display(Name = "Name")]
+        public string Username { get; set; } = "";
+        [Required(ErrorMessage = "First Name is required!")]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required!")]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Phone number is required!")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone Number!")]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Phone Number!")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; } = "";
+        [Required(ErrorMessage = "Email is required!"), EmailAddress(ErrorMessage = "Must be email format!")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessage = "Password is required!")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StrongPassword]
+        public string Password { get; set; } = "";
+        [Required(ErrorMessage = "Confirm Password is required!")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirmation does not match!")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+}
