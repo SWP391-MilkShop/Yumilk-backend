9ff44ae31a12c351f15a3035cffdce71e3d943c7
Phan Vu <anhvu2182004@gmail.com>
2024-05-17 23:26:32 +0700

    [ADD]: Thêm logic xử lí đăng nhập login service

--- NET1814_MilkShop.Services/Services/AuthenticationService.cs
+++ NET1814_MilkShop.Services/Services/AuthenticationService.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using NET1814_MilkShop.Repositories.Data.Entities;
 using NET1814_MilkShop.Repositories.Models;
 using NET1814_MilkShop.Repositories.Repositories;
 using NET1814_MilkShop.Repositories.UnitOfWork;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NET1814_MilkShop.Services.Services
 {
@@ -10,6 +14,7 @@ namespace NET1814_MilkShop.Services.Services
     {
         Task<ResponseModel> SignUpAsync(SignUpModel model);
         Task<ResponseModel> CreateUserAsync(CreateUserModel model);
+        Task<ResponseLoginModel> LoginAsync(RequestLoginModel model);
     }
 
     public sealed class AuthenticationService : IAuthenticationService
@@ -17,12 +22,17 @@ namespace NET1814_MilkShop.Services.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IUserRepository _userRepository;
         private readonly ICustomerRepository _customerRepository;
+        private readonly string Key = "qwertyuiopasdfghjklzxcvbnmasdasdasdasdasdasdasdas";
+        private readonly IAuthenticationRepository _authenticationRepository;
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
 
         public AuthenticationService(IServiceProvider serviceProvider)
         {
             _unitOfWork = serviceProvider.GetRequiredService<IUnitOfWork>();
             _userRepository = serviceProvider.GetRequiredService<IUserRepository>();
             _customerRepository = serviceProvider.GetRequiredService<ICustomerRepository>();
+            _authenticationRepository = serviceProvider.GetRequiredService<IAuthenticationRepository>();
+            _refreshTokenRepository = serviceProvider.GetRequiredService<IRefreshTokenRepository>();
         }
 
         /// <summary>
@@ -117,6 +127,68 @@ namespace NET1814_MilkShop.Services.Services
                 };
             }
             return new ResponseModel { Status = "Error", Message = "Đăng ký tài khoản thất bại" };
+        }
+        public async Task<ResponseLoginModel> LoginAsync(RequestLoginModel model)
+        {
+            var isUserExisted = await _authenticationRepository.GetUserByUserNameNPassword(model.UserName, model.Password);
+            if (isUserExisted != null)
+            {
+                var token = CreateJwtToken(isUserExisted);
+                var refreshToken = CreateJwtRefreshToken(isUserExisted);
+                return new ResponseLoginModel
+                {
+                    FirstName = isUserExisted.FirstName,
+                    LastName = isUserExisted.LastName,
+                    Token = token,
+                    RefreshToken = refreshToken,
+                    Message = "Đăng nhập thành công"
+                };
+            }
+            else
+            {
+                return new ResponseLoginModel
+                {
+                    Message = "Tên đăng nhập hoặc mật khẩu sai " + BCrypt.Net.BCrypt.HashPassword("string")
+                };
+            }
+        }
+
+        private string CreateJwtRefreshToken(User isUserExisted)
+        {
+            var randomByte = new Byte[64];
+            var token = Convert.ToBase64String(randomByte);
+            var refreshToken = new RefreshToken
+            {
+                Id = new Random().Next(0, 10000000),
+                Token = token,
+                Expires = DateTime.UtcNow.AddDays(3),
+                UserId = isUserExisted.Id,
+                CreatedAt = DateTime.UtcNow,
+                DeletedAt = DateTime.UtcNow.AddDays(3),
+            };
+            _refreshTokenRepository.Add(refreshToken);
+            return token;
+        }
+
+        private string CreateJwtToken(User isUserExisted)
+        {
+            var key = Encoding.UTF8.GetBytes(Key);
+            var securityKey = new SymmetricSecurityKey(key);
+            var credential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Audience = "",
+                Issuer = "",
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim("UserID", isUserExisted.Id.ToString()),
+                    new Claim(ClaimTypes.Role, isUserExisted.RoleId.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                SigningCredentials = credential,
+            };
+            var token = tokenHandler.CreateToken(tokenDescription);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
