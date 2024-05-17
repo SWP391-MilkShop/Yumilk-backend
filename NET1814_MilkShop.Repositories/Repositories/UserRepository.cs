c53d297e73f7951566d67d7735b00eb062bdbce2
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-17 18:17:49 +0700

    [ADD] Tạo repo xử lí + UnitOfWork

--- NET1814_MilkShop.Repositories/Repositories/UserRepository.cs
+++ NET1814_MilkShop.Repositories/Repositories/UserRepository.cs
@@ -7,6 +7,10 @@ namespace NET1814_MilkShop.Repositories.Repositories
     public interface IUserRepository
     {
         Task<List<User>> GetUsersAsync();
+        Task<User?> GetByUsernameAsync(string username);
+        void Add(User user);
+        void Update(User user);
+        void Remove(User user);
     }
     public sealed class UserRepository : Repository<User>, IUserRepository
     {
@@ -15,9 +19,14 @@ namespace NET1814_MilkShop.Repositories.Repositories
         {
         }
 
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Username == username && x.IsActive);
+        }
+
         public async Task<List<User>> GetUsersAsync()
         {
-            return await _context.Users.Where(x=> x.IsActive==true).ToListAsync();
+            return await _context.Users.Where(x=> x.IsActive).ToListAsync();
         }
     }
 }
---
+++ NET1814_MilkShop.Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using NET1814_MilkShop.Repositories.Data;
+using NET1814_MilkShop.Repositories.Repositories;
+
+namespace NET1814_MilkShop.Repositories.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
