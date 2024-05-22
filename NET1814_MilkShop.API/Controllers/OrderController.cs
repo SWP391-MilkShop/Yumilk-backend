862e74363568c3f962a2ca6683537812e3bc8dde
Phan Vu <anhvu2182004@gmail.com>
2024-05-22 19:14:36 +0700

    [ADD]: Thêm API Filter, Sort, Paging cho Order

---
+++ NET1814_MilkShop.API/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET1814_MilkShop.Repositories.Models.OrderModels;
+using NET1814_MilkShop.Services.Services;
+using ILogger = Serilog.ILogger;
+
+namespace NET1814_MilkShop.API.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly ILogger _logger;
+
+        public OrderController(IOrderService orderService, ILogger logger)
+        {
+            _orderService = orderService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders([FromQuery] OrderQueryModel queryModel)
+        {
+            _logger.Information("Get all orders");
+            var response = await _orderService.GetOrderAsync(queryModel);
+            if (response.Status == "Error")
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
