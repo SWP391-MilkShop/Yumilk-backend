bbcaaacc121ec5130ca39346829e1624b0006ab3
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-23 11:05:52 +0700

    [UPDATE] Thêm authorize admin cho order

--- NET1814_MilkShop.API/Controllers/OrderController.cs
+++ NET1814_MilkShop.API/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NET1814_MilkShop.Repositories.Models.OrderModels;
 using NET1814_MilkShop.Services.Services;
 using ILogger = Serilog.ILogger;
@@ -19,6 +20,7 @@ namespace NET1814_MilkShop.API.Controllers
         }
 
         [HttpGet]
+        [Authorize(AuthenticationSchemes = "Access", Roles = "1")]
         public async Task<IActionResult> GetOrders([FromQuery] OrderQueryModel queryModel)
         {
             _logger.Information("Get all orders");
