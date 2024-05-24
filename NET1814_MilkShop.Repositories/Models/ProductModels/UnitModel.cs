0de63d1c7bf14ed2819e18d2e11318802ff39a0a
khanght <khanghuynhtan.work@gmail.com>
2024-05-25 00:02:45 +0700

    [ADD] Thêm UnitModel nhưng không có isActive

---
+++ NET1814_MilkShop.Repositories/Models/ProductModels/UnitModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NET1814_MilkShop.Repositories.Models.ProductModels;
+
+public class UnitModel
+{
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required] 
+        public string Description { get; set; } = string.Empty;
+}
