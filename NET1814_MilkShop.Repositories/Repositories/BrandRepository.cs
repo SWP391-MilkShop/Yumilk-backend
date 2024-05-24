5c18a58dec62c636b2bfbe0569ab6b27d2f2623c
Phan Vu <anhvu2182004@gmail.com>
2024-05-24 22:04:13 +0700

    [ADD]: Thêm func GetBrandsQuery lấy toàn bộ brand dưới dạng QueryAble

--- NET1814_MilkShop.Repositories/Repositories/BrandRepository.cs
+++ NET1814_MilkShop.Repositories/Repositories/BrandRepository.cs
@@ -1,4 +1,5 @@
-﻿using NET1814_MilkShop.Repositories.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NET1814_MilkShop.Repositories.Data;
 using NET1814_MilkShop.Repositories.Data.Entities;
 using NET1814_MilkShop.Repositories.Repositories;
 
@@ -6,11 +7,21 @@ namespace NET1814_MilkShop.Repositories.Repositories
 {
     public interface IBrandRepository
     {
+        IQueryable<Brand> GetBrandsQuery();
+        void Add(Brand b);
+        void Update(Brand b);
+        void Remove(Brand b);
     }
+
     public class BrandRepository : Repository<Brand>, IBrandRepository
     {
         public BrandRepository(AppDbContext context) : base(context)
         {
+        }
+
+        public IQueryable<Brand> GetBrandsQuery()
+        {
+            return _context.Brands.AsNoTracking();
         }
     }
 }
