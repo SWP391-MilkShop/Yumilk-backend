57928e8a815f9827e9af7e9b49de8e621093f1a4
Phan Vu <anhvu2182004@gmail.com>
2024-05-24 22:03:02 +0700

    [ADD]: Thêm BrandModel

---
+++ NET1814_MilkShop.Repositories/Models/BrandModels/BrandModel.cs
@@ -0,0 +1,10 @@
+namespace NET1814_MilkShop.Repositories.Models.BrandModels;
+
+public class BrandModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+}
