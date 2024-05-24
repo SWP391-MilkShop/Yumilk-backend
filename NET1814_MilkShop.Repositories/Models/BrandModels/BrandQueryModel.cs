0f1f3b63b9a3286ee020baa174fed6e165d459f6
Phan Vu <anhvu2182004@gmail.com>
2024-05-24 22:22:48 +0700

    [UPDATE]: Description được null

--- NET1814_MilkShop.Repositories/Models/BrandModels/BrandQueryModel.cs
+++ NET1814_MilkShop.Repositories/Models/BrandModels/BrandQueryModel.cs
@@ -2,5 +2,5 @@ namespace NET1814_MilkShop.Repositories.Models.BrandModels;
 
 public class BrandQueryModel : QueryModel
 {
-    public string Description { get; set; }
+    public string? Description { get; set; }
 }
