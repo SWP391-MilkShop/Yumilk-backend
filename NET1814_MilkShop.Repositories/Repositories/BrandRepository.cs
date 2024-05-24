c80fe8c4a578014a8511fe1486135efec6275a35
Phan Vu <anhvu2182004@gmail.com>
2024-05-24 23:41:19 +0700

    [ADD]: Thêm 2 chức năng lấy brand bằng id và name

--- NET1814_MilkShop.Repositories/Repositories/BrandRepository.cs
+++ NET1814_MilkShop.Repositories/Repositories/BrandRepository.cs
@@ -11,6 +11,8 @@ namespace NET1814_MilkShop.Repositories.Repositories
         void Add(Brand b);
         void Update(Brand b);
         void Remove(Brand b);
+        Task<Brand?> GetById(int id);
+        Task<Brand?> GetBrandByName(string name);
     }
 
     public class BrandRepository : Repository<Brand>, IBrandRepository
@@ -22,6 +24,11 @@ namespace NET1814_MilkShop.Repositories.Repositories
         public IQueryable<Brand> GetBrandsQuery()
         {
             return _context.Brands.AsNoTracking();
+        }
+
+        public async Task<Brand?> GetBrandByName(string name)
+        {
+            return await _context.Brands.AsNoTracking().FirstOrDefaultAsync(x => string.Equals(x.Name, name));
         }
     }
 }
