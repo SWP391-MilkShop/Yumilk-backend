a2c843251a550cd78462b4f0ab94aceafad695d0
khanght <khanghuynhtan.work@gmail.com>
2024-05-24 11:31:24 +0700

    [UPDATE] Uncomment UseEndpoints để check live server

--- NET1814_MilkShop.API/Startup.cs
+++ NET1814_MilkShop.API/Startup.cs
@@ -169,10 +169,10 @@ namespace NET1814_MilkShop.API
             app.UseAuthorization();
             app.UseExceptionHandler(options => { });
             // ko biet sao cai nay no keu violate ASP0014, keu map route truc tiep trong api luon
-            //app.UseEndpoints(endpoint =>
-            //{
-            //    endpoint.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
             app.MapControllers();
         }
 
