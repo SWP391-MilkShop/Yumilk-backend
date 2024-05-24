732feed84777435d553ec78d63ee9bbd244b8d73
Phan Vu <anhvu2182004@gmail.com>
2024-05-24 23:42:26 +0700

    [ADD]: Thêm annotation require cho name trong brandmodel

--- NET1814_MilkShop.Repositories/Models/BrandModels/BrandModel.cs
+++ NET1814_MilkShop.Repositories/Models/BrandModels/BrandModel.cs
@@ -1,10 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NET1814_MilkShop.Repositories.Models.BrandModels;
 
 public class BrandModel
 {
     public int Id { get; set; }
-
-    public string Name { get; set; } = null!;
+    [Required] public string Name { get; set; }
 
     public string? Description { get; set; }
 }
