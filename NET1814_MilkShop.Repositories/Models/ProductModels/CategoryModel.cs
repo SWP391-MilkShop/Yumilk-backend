9f4a6ac8e764af22e9c185a53f554c4335a32169
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-25 07:53:44 +0700

    [ADD] Tạo category model và query model

---
+++ NET1814_MilkShop.Repositories/Models/ProductModels/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NET1814_MilkShop.Repositories.Models.ProductModels
+{
+    public class CategoryModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ NET1814_MilkShop.Repositories/Models/ProductModels/CategoryQueryModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NET1814_MilkShop.Repositories.Models.ProductModels
+{
+    public class CategoryQueryModel : QueryModel
+    {
+        public string? Name { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
