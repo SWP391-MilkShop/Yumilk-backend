b26646ebef980e62cf78db22716da202e46103d5
Phan Vu <anhvu2182004@gmail.com>
2024-05-25 18:55:34 +0700

    [UPDATE]: Thêm 3 thuộc tính và description xml cho brand query model

--- NET1814_MilkShop.Repositories/Models/BrandModels/BrandQueryModel.cs
+++ NET1814_MilkShop.Repositories/Models/BrandModels/BrandQueryModel.cs
@@ -2,5 +2,15 @@ namespace NET1814_MilkShop.Repositories.Models.BrandModels;
 
 public class BrandQueryModel : QueryModel
 {
-    public string? Description { get; set; }
+    public bool IsActive { get; set; } = true;
+
+    /// <summary>
+    /// Search by name or description
+    /// </summary>
+    public new string? SearchTerm { get; set; }
+
+    /// <summary>
+    /// Sort by id or name (default is id)
+    /// </summary>
+    public new string? SortColumn { get; set; }
 }
