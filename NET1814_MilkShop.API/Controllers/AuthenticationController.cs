a04f7d44f9f1108f9f93850ab51360fb2c282f84
khanght <khanghuynhtan.work@gmail.com>
2024-05-26 19:22:09 +0700

    [FORMAT] format code

--- NET1814_MilkShop.API/Controllers/AuthenticationController.cs
+++ NET1814_MilkShop.API/Controllers/AuthenticationController.cs
@@ -20,7 +20,7 @@ namespace NET1814_MilkShop.API.Controllers
         }
 
         [HttpPost("create-user")]
-        [Authorize(AuthenticationSchemes ="Access", Roles = "1")]
+        [Authorize(AuthenticationSchemes = "Access", Roles = "1")]
         public async Task<IActionResult> CreateUser([FromBody] CreateUserModel model)
         {
             _logger.Information("Create user");
@@ -72,7 +72,7 @@ namespace NET1814_MilkShop.API.Controllers
 
             return Ok(res);
         }
-        
+
         /// <summary>
         ///  Only Admin role can login, others will say wrong username or password.
         /// </summary>
