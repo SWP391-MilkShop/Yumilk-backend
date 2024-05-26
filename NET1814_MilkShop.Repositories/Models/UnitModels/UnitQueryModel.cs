5f0d06a2b3f5aaffc0c1c150182d78accbf443e5
khanght <khanghuynhtan.work@gmail.com>
2024-05-26 15:40:17 +0700

    [ADD] Thêm IsActive cho UnitQueryModel

--- NET1814_MilkShop.Repositories/Models/UnitModels/UnitQueryModel.cs
+++ NET1814_MilkShop.Repositories/Models/UnitModels/UnitQueryModel.cs
@@ -3,4 +3,5 @@ namespace NET1814_MilkShop.Repositories.Models.UnitModels;
 public class UnitQueryModel : QueryModel
 {
     //SearchTerm se search theo Name, Description
+    public bool? IsActive { get; set; }
 }
