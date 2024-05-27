252ac8710002b939623c90753dfa5f0df130af20
Phan Vu <anhvu2182004@gmail.com>
2024-05-27 10:46:13 +0700

    [UPDATE]: Thêm token type reset

--- NET1814_MilkShop.Services/CoreHelpers/Extensions/JwtTokenExtension.cs
+++ NET1814_MilkShop.Services/CoreHelpers/Extensions/JwtTokenExtension.cs
@@ -11,7 +11,8 @@ namespace NET1814_MilkShop.Services.CoreHelpers.Extensions
     {
         Access,
         Refresh,
-        Authentication
+        Authentication,
+        Reset
     }
 
     public interface IJwtTokenExtension
@@ -62,6 +63,9 @@ namespace NET1814_MilkShop.Services.CoreHelpers.Extensions
                 case TokenType.Authentication:
                     claims.Add(new Claim("Token", user.VerificationCode));
                     break;
+                case TokenType.Reset:
+                    claims.Add(new Claim("Token", user.ResetPasswordCode));
+                    break;
                 default:
                     throw new ArgumentException("Invalid token type");
             }
@@ -82,6 +86,9 @@ namespace NET1814_MilkShop.Services.CoreHelpers.Extensions
                 case TokenType.Authentication:
                     key = _configuration["Jwt:AuthenticationKey"];
                     break;
+                case TokenType.Reset:
+                    key = _configuration["Jwt:AuthenticationKey"];
+                    break;
                 default:
                     throw new ArgumentException("Invalid token type");
             }
@@ -100,6 +107,9 @@ namespace NET1814_MilkShop.Services.CoreHelpers.Extensions
                     expiry = int.Parse(_configuration["Jwt:RefreshTokenLifeTime"]);
                     break;
                 case TokenType.Authentication:
+                    expiry = int.Parse(_configuration["Jwt:AuthenticationLifeTime"]);
+                    break;
+                case TokenType.Reset:
                     expiry = int.Parse(_configuration["Jwt:AuthenticationLifeTime"]);
                     break;
                 default:
