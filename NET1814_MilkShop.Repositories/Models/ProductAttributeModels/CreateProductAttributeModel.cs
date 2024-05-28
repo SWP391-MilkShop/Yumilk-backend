447d07a7cf622cda4c46c989090b514eabc1667a
Phan Vu <anhvu2182004@gmail.com>
2024-05-28 17:16:49 +0700

    [UPDATE]: Thêm thuộc tính isactive

--- NET1814_MilkShop.Repositories/Models/ProductAttributeModels/CreateProductAttributeModel.cs
+++ NET1814_MilkShop.Repositories/Models/ProductAttributeModels/CreateProductAttributeModel.cs
@@ -5,6 +5,8 @@ namespace NET1814_MilkShop.Repositories.Models.ProductAttributeModels;
 public class CreateProductAttributeModel
 {
     [Required(ErrorMessage = "Product attribute name is required")]
-    public string Name { get; set; }
+    public string Name { get; set; } = null;
     public string? Description { get; set; }
+
+    public Boolean IsActive { get; set; } = true;
 }
