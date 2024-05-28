a25b46191ac252739922724c00006a7bd9672b3e
khanght <khanghuynhtan.work@gmail.com>
2024-05-28 19:51:53 +0700

    [ADD + UPDATE] Thêm CRUD address và yml comment api để note và sửa một số api endpoint

--- NET1814_MilkShop.API/Controllers/UserController.cs
+++ NET1814_MilkShop.API/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NET1814_MilkShop.API.CoreHelpers.ActionFilters;
+using NET1814_MilkShop.Repositories.Models.AddressModels;
 using NET1814_MilkShop.Repositories.Models.UserModels;
 using NET1814_MilkShop.Services.Services;
 using ILogger = Serilog.ILogger;
@@ -15,14 +16,16 @@ namespace NET1814_MilkShop.API.Controllers
         private readonly ILogger _logger;
         private readonly IUserService _userService;
         private readonly ICustomerService _customerService;
+        private readonly IAddressService _addressService;
 
         public UserController(ILogger logger, IServiceProvider serviceProvider)
         {
             _logger = logger;
             _userService = serviceProvider.GetRequiredService<IUserService>();
             _customerService = serviceProvider.GetRequiredService<ICustomerService>();
+            _addressService = serviceProvider.GetRequiredService<IAddressService>();
         }
-
+        #region User
         [HttpGet]
         [Route("api/users")]
         [Authorize(AuthenticationSchemes = "Access", Roles = "1")]
@@ -37,7 +40,9 @@ namespace NET1814_MilkShop.API.Controllers
             }
             return Ok(response);
         }
+        #endregion
 
+        #region  Customer
         [HttpGet]
         [Route("api/customers")]
         [Authorize(AuthenticationSchemes = "Access", Roles = "1")]
@@ -67,9 +72,12 @@ namespace NET1814_MilkShop.API.Controllers
             }
             return Ok(response);
         }
+        #endregion
 
+        #region Account
         [HttpGet]
-        [Route("api/user/me")]
+        /*[Route("api/user/me")]*/
+        [Route("api/user/account/profile")]
         [Authorize(AuthenticationSchemes = "Access")]
         [ServiceFilter(typeof(UserExistsFilter))]
         public async Task<IActionResult> GetCurrentAuthUser()
@@ -83,9 +91,14 @@ namespace NET1814_MilkShop.API.Controllers
             }
             return Ok(response);
         }
-
+        /// <summary>
+        /// Only Customer can change profile info?
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPut]
-        [Route("api/user/change-info")]
+        /*[Route("api/user/account/change-info")]*/
+        [Route("api/user/account/profile")]
         [Authorize(AuthenticationSchemes = "Access", Roles = "3")]
         [ServiceFilter(typeof(UserExistsFilter))]
         public async Task<IActionResult> ChangeInfo([FromBody] ChangeUserInfoModel model)
@@ -101,7 +114,8 @@ namespace NET1814_MilkShop.API.Controllers
         }
 
         [HttpPut]
-        [Route("api/user/change-password")]
+        /*[Route("api/user/change-password")]*/
+        [Route("api/user/account/change-password")]
         [Authorize(AuthenticationSchemes = "Access")]
         [ServiceFilter(typeof(UserExistsFilter))]
         public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
@@ -115,5 +129,92 @@ namespace NET1814_MilkShop.API.Controllers
             }
             return Ok(response);
         }
+        /// <summary>
+        /// Feature only available for Customer role
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/user/account/address")]
+        [Authorize(AuthenticationSchemes = "Access",Roles = "3")]
+        [ServiceFilter(typeof(UserExistsFilter))]
+        public async Task<IActionResult> GetCustomerAddresses()
+        {
+            _logger.Information("Get customer addresses");
+            var userId = (HttpContext.Items["UserId"] as Guid?)!.Value;
+            var response = await _addressService.GetAddressesByCustomerId(userId);
+            if (response.Status == "Error")
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+        
+        /// <summary>
+        /// Feature only available for Customer role,
+        /// max 3 addresses and cannot set first address to non-default
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        /*[Route("api/user/change-password")]*/
+        [Route("api/user/account/address")]
+        [Authorize(AuthenticationSchemes = "Access",Roles = "3")]
+        [ServiceFilter(typeof(UserExistsFilter))]
+        public async Task<IActionResult> CreateCustomerAddress([FromBody] CreateAddressModel request)
+        {
+            _logger.Information("Create customer address");
+            var customerId = (HttpContext.Items["UserId"] as Guid?)!.Value;
+            var response = await _addressService.CreateAddressAsync(customerId, request);
+            if (response.Status == "Error")
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Feature only available for Customer role,
+        /// if customer has only 1 address then cannot set it to non-default
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("api/user/account/address/{id}")]
+        [Authorize(AuthenticationSchemes = "Access", Roles = "3")]
+        [ServiceFilter(typeof(UserExistsFilter))]
+        public async Task<IActionResult> UpdateCustomerAddress(
+            int id,
+            [FromBody] UpdateAddressModel request)
+        {
+            _logger.Information("Update Customer Address");
+            var customerId = (HttpContext.Items["UserId"] as Guid?)!.Value;
+            var response = await _addressService.UpdateAddressAsync(customerId,id,request);
+            if (response.Status == "Error")
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        /// <summary>
+        /// feature only available for Customer role,
+        /// cannot delete with address that is default
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("api/user/account/address/{id}")]
+        [Authorize(AuthenticationSchemes = "Access", Roles = "3")]
+        [ServiceFilter(typeof(UserExistsFilter))]
+        public async Task<IActionResult> DeleteCustomerAddress(int id)
+        {
+            _logger.Information("Delete Customer Address");
+            var customerId = (HttpContext.Items["UserId"] as Guid?)!.Value;
+            var response = await _addressService.DeleteAddressAsync(customerId, id);
+            if (response.Status == "Error")
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        #endregion
     }
 }
