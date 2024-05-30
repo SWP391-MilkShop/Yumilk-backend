35e02159176772b7f8706e4965e226dbdb433544
khanght <khanghuynhtan.work@gmail.com>
2024-05-30 11:21:18 +0700

    [TEST]  app.UseDeveloperExceptionPage() in production to fix bug temporary

--- NET1814_MilkShop.API/Startup.cs
+++ NET1814_MilkShop.API/Startup.cs
@@ -158,8 +158,9 @@ namespace NET1814_MilkShop.API
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                
             }
+            app.UseDeveloperExceptionPage();
 
             var isUserSwagger = _configuration.GetValue<bool>("UseSwagger", false);
             if (isUserSwagger)
