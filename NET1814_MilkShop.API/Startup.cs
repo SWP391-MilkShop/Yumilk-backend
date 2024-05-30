34b72f7e5af172bce26486b2567e6ca729ae9c88
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-05-30 22:04:59 +0700

    [ADD] Thêm repo, service, controller để lưu product images

--- NET1814_MilkShop.API/Startup.cs
+++ NET1814_MilkShop.API/Startup.cs
@@ -223,6 +223,9 @@ namespace NET1814_MilkShop.API
             services.AddScoped<IProductAttributeValueRepository, ProductAttributeValueRepository>();
             services.AddScoped<IProductAttributeValueService, ProductAttributeValueService>();
 
+            services.AddScoped<IProductImageRepository, ProductImageRepository>();
+            services.AddScoped<IProductImageService, ProductImageService>();
+
             services.AddScoped<IAddressRepository, AddressRepository>();
             services.AddScoped<IAddressService, AddressService>();
 
