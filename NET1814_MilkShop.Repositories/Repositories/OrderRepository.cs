5f437cdb10cf8c6a64af952c041783366f11acbf
Phan Vu <anhvu2182004@gmail.com>
2024-06-04 12:01:33 +0700

    [ADD]: Thêm addrange func trong orderrepo

--- NET1814_MilkShop.Repositories/Repositories/OrderRepository.cs
+++ NET1814_MilkShop.Repositories/Repositories/OrderRepository.cs
@@ -7,6 +7,9 @@ namespace NET1814_MilkShop.Repositories.Repositories
     public interface IOrderRepository
     {
         IQueryable<Order> GetOrdersQuery();
+        void Add(Order order);
+        void AddRange(IEnumerable<OrderDetail> list);
+       
     }
 
     public class OrderRepository : Repository<Order>, IOrderRepository
@@ -19,6 +22,16 @@ namespace NET1814_MilkShop.Repositories.Repositories
             //return _context.Orders.Include(o => o.Status).Include(o => o.Customer).AsNoTracking();
             return _query.Include(o => o.Status)
                          .Include(o => o.Customer);
+        }
+
+        public void AddRange(IEnumerable<OrderDetail> list)
+        {
+            _context.OrderDetails.AddRange(list);
+        }
+
+        public void Remove(OrderDetail orderDetail)
+        {
+            _context.OrderDetails.RemoveRange();
         }
     }
 }
