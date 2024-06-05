b214b046360b21454f16e3df906d851562119895
Phan Vu <anhvu2182004@gmail.com>
2024-06-05 12:07:52 +0700

    [UPDATE]: cập nhật attribute cho phonenumber

--- NET1814_MilkShop.Repositories/Models/CheckoutModels/CheckoutModel.cs
+++ NET1814_MilkShop.Repositories/Models/CheckoutModels/CheckoutModel.cs
@@ -5,8 +5,9 @@ namespace NET1814_MilkShop.Repositories.Models.CheckoutModels;
 public class CheckoutModel
 {
     public decimal ShippingFee { get; set; }
-    [Required] public string? Address { get; set; }
-    [Required] public string? PhoneNumber { get; set; }
+
+    [Required] public string Address { get; set; } = null;
+    [Required] [Phone] public string PhoneNumber { get; set; } = null;
     public string? Note { get; set; }
     [Required] public string? PaymentMethod { get; set; }
 }
