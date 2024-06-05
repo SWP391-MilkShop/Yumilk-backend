90b2fe8d4b76831199d852519dcce6d608b74dc8
Phan Vu <anhvu2182004@gmail.com>
2024-06-05 17:20:51 +0700

    [ADD]: Thêm các trường dữ liệu cho checkoutresponsemodel

--- NET1814_MilkShop.Repositories/Models/CheckoutModels/CheckoutResponseModel.cs
+++ NET1814_MilkShop.Repositories/Models/CheckoutModels/CheckoutResponseModel.cs
@@ -2,6 +2,13 @@ namespace NET1814_MilkShop.Repositories.Models.CheckoutModels;
 
 public class CheckoutResponseModel
 {
-    public string ProductName { get; set; }
-    public int Quantity { get; set; }
+    public Guid? OrderId { get; set; }
+    public Guid? CustomerId { get; set; }
+    public string? FullName { get; set; }
+    public decimal TotalAmount { get; set; }
+    public decimal ShippingFee { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Note { get; set; }
+    public object? OrderDetail { get; set; }
 }
