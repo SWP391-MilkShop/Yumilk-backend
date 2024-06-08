a3674a13ac3fac951063471792d5babbd628a91e
Phan Vu <anhvu2182004@gmail.com>
2024-06-08 19:28:22 +0700

    [ADD]: Thêm 2 func repo cho orderhistory

--- NET1814_MilkShop.Repositories/Repositories/OrderRepository.cs
+++ NET1814_MilkShop.Repositories/Repositories/OrderRepository.cs
@@ -7,21 +7,33 @@ namespace NET1814_MilkShop.Repositories.Repositories
     public interface IOrderRepository
     {
         IQueryable<Order> GetOrdersQuery();
-
+        IQueryable<Order> GetOrderHistory(Guid customerId);
         void Add(Order order);
         void AddRange(IEnumerable<OrderDetail> list);
         Task<Order?> GetByCodeAsync(int orderCode);
+        Task<Order?> GetByOrderIdAsync(Guid orderId);
     }
 
     public class OrderRepository : Repository<Order>, IOrderRepository
     {
         public OrderRepository(AppDbContext context)
-            : base(context) { }
+            : base(context)
+        {
+        }
 
         public IQueryable<Order> GetOrdersQuery()
         {
             //return _context.Orders.Include(o => o.Status).Include(o => o.Customer).AsNoTracking();
-            return _query.Include(o => o.Status).Include(o => o.Customer);
+            return _query.Include(o => o.Status)
+                .Include(o => o.Customer)
+                .Include(o => o.OrderDetails);
+        }
+
+        public IQueryable<Order> GetOrderHistory(Guid customerId)
+        {
+            return _query.Include(o => o.OrderDetails).ThenInclude(o => o.Product)
+                .Include(o => o.Status)
+                .Where(x => x.CustomerId == customerId);
         }
 
         public void AddRange(IEnumerable<OrderDetail> list)
@@ -38,7 +50,17 @@ namespace NET1814_MilkShop.Repositories.Repositories
                 .Include(o => o.OrderDetails)
                 .ThenInclude(o => o.Product)
                 .FirstOrDefaultAsync(o => o.OrderCode == orderCode);
+        }
 
+        public async Task<Order?> GetByOrderIdAsync(Guid orderId)
+        {
+            return await _query
+                .Include(o => o.Status)
+                .Include(o => o.Customer)
+                .ThenInclude(o => o.User)
+                .Include(o => o.OrderDetails)
+                .ThenInclude(o => o.Product)
+                .FirstOrDefaultAsync(o => o.Id == orderId);
         }
     }
 }
