40253837a759ee5fd9c23d69bc57f9e36935b4d0
Phan Vu <anhvu2182004@gmail.com>
2024-06-08 19:27:52 +0700

    [ADD]: Thêm order history query cho người dùng

---
+++ NET1814_MilkShop.Repositories/Models/OrderModels/OrderHistoryQueryModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NET1814_MilkShop.Repositories.Models.OrderModels;
+
+public class OrderHistoryQueryModel : QueryModel
+{
+    [Range(
+        typeof(decimal),
+        "0",
+        "79228162514264337593543950335",
+        ErrorMessage = "Total amount must be >= 0"
+    )]
+    public decimal TotalAmount { get; set; } = 0;
+
+    public DateTime? FromOrderDate { get; set; }
+    public DateTime? ToOrderDate { get; set; }
+
+    [Range(1, 5, ErrorMessage = "Status Id must be in range 1-5")]
+    public int? OrderStatus { get; set; }
+
+    /// <summary>
+    /// search theo tên sản phẩm trong order history
+    /// </summary>
+    public new string? SearchTerm { get; set; }
+}
