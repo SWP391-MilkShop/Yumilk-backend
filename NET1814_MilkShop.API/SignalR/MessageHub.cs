b55935258bd66533a3891537413d60d916d3434d
Phan Vu <anhvu2182004@gmail.com>
2024-07-03 12:08:15 +0700

    [ADD]: Thêm hub và repo xử lí message khi có người dùng connect hoặc disconnect

--- NET1814_MilkShop.API/SignalR/MessageHub.cs
+++ NET1814_MilkShop.API/SignalR/MessageHub.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using NET1814_MilkShop.Repositories.CoreHelpers.Enum;
 using NET1814_MilkShop.Repositories.Data.Entities;
 using NET1814_MilkShop.Repositories.Models.MessageModels;
 using NET1814_MilkShop.Repositories.Repositories;
 using NET1814_MilkShop.Repositories.UnitOfWork;
+using NET1814_MilkShop.Services.Services;
 using ILogger = Serilog.ILogger;
 
 namespace NET1814_MilkShop.API.SignalR;
@@ -15,14 +17,16 @@ public class MessageHub : Hub
     private readonly ILogger _logger;
     private readonly IUserRepository _userRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMessageService _messageService;
 
     public MessageHub(IMessageRepository messageRepository, ILogger logger, IUserRepository userRepository,
-        IUnitOfWork unitOfWork)
+        IUnitOfWork unitOfWork, IMessageService messageService)
     {
         _messageRepository = messageRepository;
         _logger = logger;
         _userRepository = userRepository;
         _unitOfWork = unitOfWork;
+        _messageService = messageService;
     }
 
     public override async Task OnConnectedAsync()
@@ -31,21 +35,31 @@ public class MessageHub : Hub
         var httpContext = Context.GetHttpContext();
         var currentUser = Guid.Parse(httpContext.User.Claims.FirstOrDefault(x => x.Type == "UserId")?.Value);
         var otherUser = Guid.Parse(httpContext.Request.Query["otherUser"].ToString());
+        var isAdmin = await _userRepository.GetByIdAsync(otherUser);
+        if (isAdmin.RoleId == (int)RoleId.CUSTOMER)
+        {
+            throw new HubException("Bạn chỉ có thể gửi tin nhắn cho nhân viên");
+        }
+
         var groupName = GetGroupName(currentUser, otherUser);
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-        var messages = await _messageRepository.GetMessageThread(currentUser, otherUser);
-        await Clients.Groups(groupName).SendAsync("ReceiveMessageThread", messages);
+        var group = await AddToGroup(groupName);
+        await Clients.Group(groupName).SendAsync("UpdatedGroup", group);
+        var messages = await _messageService.GetMessageThread(currentUser, otherUser);
+        await Clients.Caller.SendAsync("ReceiveMessageThread", messages);
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
+        var group = await RemoveFromMessageGroup();
+        await Clients.Group(group.Name).SendAsync("UpdatedGroup", group);
         await base.OnDisconnectedAsync(exception);
     }
 
     public async Task SendMessage(CreateMessageModel createMessageModel)
     {
         var httpContext = Context.GetHttpContext();
-        var senderId = (httpContext!.Items["UserId"] as Guid?)!.Value;
+        var senderId = Guid.Parse(httpContext.User.Claims.FirstOrDefault(x => x.Type == "UserId")?.Value);
         var senderExist = await _userRepository.GetByIdAsync(senderId);
         if (senderExist == null)
         {
@@ -78,6 +92,13 @@ public class MessageHub : Hub
             Content = createMessageModel.Content,
         };
         _messageRepository.Add(message);
+        var groupName = GetGroupName(senderId, createMessageModel.RecipientId);
+        var group = await _messageRepository.GetMessageGroup(groupName);
+        if (group!.Connections.Any(x => x.UserId == createMessageModel.RecipientId))
+        {
+            message.DateRead = DateTime.Now;
+        }
+
         var result = await _unitOfWork.SaveChangesAsync();
         if (result > 0)
         {
@@ -91,9 +112,52 @@ public class MessageHub : Hub
                 Content = message.Content,
                 CreatedAt = message.CreatedAt
             };
-            var group = GetGroupName(senderId, createMessageModel.RecipientId);
-            await Clients.Groups(group).SendAsync("NewMessage", resp);
+
+            await Clients.Groups(groupName).SendAsync("NewMessage", resp);
         }
+
+        throw new HubException("Đã xảy ra lỗi trong quá trình gửi tin nhắn");
+    }
+
+    private async Task<Group> AddToGroup(string groupName)
+    {
+        var group = await _messageRepository.GetMessageGroup(groupName);
+        var connection = new Connection
+        {
+            ConnectionId = Context.ConnectionId,
+            UserId = Guid.Parse(Context.User.Claims.FirstOrDefault(x => x.Type == "UserId")?.Value)
+        };
+        if (group == null)
+        {
+            group = new Group
+            {
+                Name = groupName,
+            };
+            _messageRepository.AddGroup(group);
+        }
+
+        group.Connections.Add(connection);
+        var result = await _unitOfWork.SaveChangesAsync();
+        if (result > 0)
+        {
+            return group;
+        }
+
+        throw new HubException("Tham gia cuộc trò chuyện thất bại");
+    }
+
+    private async Task<Group> RemoveFromMessageGroup()
+    {
+        var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+        var connection = group!.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+        _messageRepository.RemoveConnection(connection!);
+        var res = await _unitOfWork.SaveChangesAsync();
+        if (res > 0)
+        {
+            return group;
+        }
+
+        throw new HubException("Rời khỏi cuộc trò chuyện thất bại");
     }
 
     private string GetGroupName(Guid currentUser, Guid otherUser)
--- NET1814_MilkShop.Repositories/Repositories/MessageRepository.cs
+++ NET1814_MilkShop.Repositories/Repositories/MessageRepository.cs
@@ -12,6 +12,11 @@ public interface IMessageRepository
     void Update(Message message);
     Task<Message?> GetMessageById(Guid id);
     Task<List<Message>> GetMessageThread(Guid currentUserId, Guid recipientUserId);
+    void AddGroup(Group group);
+    void RemoveConnection(Connection connection);
+    Task<Connection?> GetConnection(string connectionId);
+    Task<Group?> GetMessageGroup(string groupName);
+    Task<Group?> GetGroupForConnection(string connectionId);
 }
 
 public class MessageRepository : Repository<Message>, IMessageRepository
@@ -32,5 +37,32 @@ public class MessageRepository : Repository<Message>, IMessageRepository
                 x.SenderId == recipientUserId && x.RecipientId == currentUserId)
             .OrderBy(x => x.CreatedAt)
             .ToListAsync();
+    }
+
+    public void AddGroup(Group group)
+    {
+        _context.Groups.Add(group);
+    }
+
+    public void RemoveConnection(Connection connection)
+    {
+        _context.Connections.Remove(connection);
+    }
+
+    public async Task<Connection?> GetConnection(string connectionId)
+    {
+        return await _context.Connections.FindAsync(connectionId);
+    }
+
+    public async Task<Group?> GetMessageGroup(string groupName)
+    {
+        return await _context.Groups.Include(x => x.Connections)
+            .FirstOrDefaultAsync(x => x.Name == groupName);
+    }
+
+    public async Task<Group?> GetGroupForConnection(string connectionId)
+    {
+        return await _context.Groups.Include(x => x.Connections).FirstOrDefaultAsync(x =>
+            x.Connections.Any(c => c.ConnectionId == connectionId));
     }
 }
