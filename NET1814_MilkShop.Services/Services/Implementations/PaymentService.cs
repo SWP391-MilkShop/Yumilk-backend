2e56b19a5f2e401f55f090e4d85285acded6aa4f
khanght <khanghuynhtan.work@gmail.com>
2024-07-18 15:10:51 +0700

    [UPDATE]: Check signature

--- NET1814_MilkShop.Services/Services/Implementations/PaymentService.cs
+++ NET1814_MilkShop.Services/Services/Implementations/PaymentService.cs
@@ -2,6 +2,7 @@ using System.Configuration;
 using Microsoft.Extensions.Configuration;
 using Net.payOS;
 using Net.payOS.Types;
+using Net.payOS.Utils;
 using NET1814_MilkShop.Repositories.Models;
 using NET1814_MilkShop.Repositories.Repositories.Interfaces;
 using NET1814_MilkShop.Services.Services.Interfaces;
@@ -148,6 +149,13 @@ public class PaymentService : IPaymentService
            var responseBodyJson = JObject.Parse(responseContent);
            var code = responseBodyJson["code"]?.ToString();
            var data = responseBodyJson["data"]?.ToString();
+           string paymentLinkResSignature = SignatureControl.CreateSignatureFromObj(responseBodyJson, _configuration["PayOS:CheckSumKey"]!);
+           if (paymentLinkResSignature != responseBodyJson["signature"]!.ToString())
+           {
+               return ResponseModel.Error("Signature không hợp lệ: paymentLinkResSignature(our server):"
+                   + paymentLinkResSignature+"\nPayOSSignature:"+responseBodyJson["signature"]);
+           }
+               
            if (code == null && code != "00")
            {
                return ResponseModel.Error("Có lỗi trong quá trình lấy dữ liệu thông tin thanh toán");
