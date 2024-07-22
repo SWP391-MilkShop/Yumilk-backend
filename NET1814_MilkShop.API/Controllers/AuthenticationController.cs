6d62579d71ab10161a15af6a90b6911c19e322ff
barooon165 <112069172+onbao165@users.noreply.github.com>
2024-07-22 20:13:29 +0700

    [ADD] Add comment for authentication controller

--- NET1814_MilkShop.API/Controllers/AuthenticationController.cs
+++ NET1814_MilkShop.API/Controllers/AuthenticationController.cs
@@ -23,6 +23,12 @@ public class AuthenticationController : Controller
         _authenticationService = serviceProvider.GetRequiredService<IAuthenticationService>();
     }
 
+    /// <summary>
+    /// Validate username, số đt, email tồn tại
+    /// <para>Nếu thành công thì lưu token vào db và gửi mail xác thực</para>
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns></returns>
     [HttpPost("sign-up")]
     public async Task<IActionResult> SignUp([FromBody] SignUpModel model)
     {
@@ -31,6 +37,11 @@ public class AuthenticationController : Controller
         return ResponseExtension.Result(response);
     }
 
+    /// <summary>
+    /// Nhận token qua query và xác thực với token lưu trong db
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
     [HttpPost("verify")]
     public async Task<IActionResult> VerifyAccount([FromQuery] string token)
     {
@@ -41,6 +52,7 @@ public class AuthenticationController : Controller
 
     /// <summary>
     /// Only customer role can login, others will say wrong username or password.
+    /// <para> Nếu bị ban thì chặn ko cho login</para>
     /// </summary>
     /// <param name="model"></param>
     /// <returns></returns>
@@ -54,6 +66,7 @@ public class AuthenticationController : Controller
 
     /// <summary>
     ///  Only Admin,Staff role can login, others will say wrong username or password.
+    /// <para> Nếu bị ban thì chặn ko cho login</para>
     /// </summary>
     /// <param name="model"></param>
     /// <returns></returns>
@@ -65,6 +78,11 @@ public class AuthenticationController : Controller
         return ResponseExtension.Result(response);
     }
 
+    /// <summary>
+    /// Validate nếu email tồn tại, lưu token vào db và gửi mail reset password
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     [HttpPost("forgot-password")]
     public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordModel request)
     {
@@ -73,6 +91,11 @@ public class AuthenticationController : Controller
         return ResponseExtension.Result(response);
     }
 
+    /// <summary>
+    /// Gửi token qua body kèm với password mới, nếu token đúng thì update password mới
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     [HttpPost("reset-password")]
     public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordModel request)
     {
@@ -81,6 +104,11 @@ public class AuthenticationController : Controller
         return ResponseExtension.Result(response);
     }
 
+    /// <summary>
+    /// Gửi token qua query, nếu token đúng thì tạo access token và refresh token mới
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
     [HttpPost("refresh-token")]
     public async Task<IActionResult> RefreshToken([FromQuery] string token)
     {
@@ -89,6 +117,11 @@ public class AuthenticationController : Controller
         return ResponseExtension.Result(response);
     }
 
+    /// <summary>
+    /// Gửi email qua query, nếu email tồn tại tạo token lưu db và gửi mail xác thực
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns></returns>
     [HttpPost("activate-account")]
     public async Task<IActionResult> ActivateAccount([FromQuery] string email)
     {
@@ -97,6 +130,15 @@ public class AuthenticationController : Controller
         return ResponseExtension.Result(response);
     }
 
+    /// <summary>
+    /// Gửi token qua query, check token với project id tạo trên firebase
+    /// <para>decode token và deserialize ra đc các info về user</para>
+    /// <para>Nếu email_verified = false yêu cầu xác thực tài khoản google</para>
+    /// <para>Nếu chưa có acc thì tự tạo và kích hoạt acc, gửi generated username + password</para>
+    /// <para>Nếu có acc thì gán google_id nếu chưa có + kích hoạt nếu chưa kích hoạt</para>
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
     [HttpPost("google-login")]
     public async Task<IActionResult> GoogleLogin([FromQuery] string token)
     {
