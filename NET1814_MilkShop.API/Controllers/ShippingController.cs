5fa7d785b961c9f31130d19bbd951bedd7e8b7ce
khanght <khanghuynhtan.work@gmail.com>
2024-07-23 09:53:09 +0700

    [ADD]: Add document/comment about validation and briefly explain in controller endpoint

--- NET1814_MilkShop.API/Controllers/ShippingController.cs
+++ NET1814_MilkShop.API/Controllers/ShippingController.cs
@@ -19,7 +19,11 @@ public class ShippingController : ControllerBase
         _shippingService = serviceProvider.GetRequiredService<IShippingService>();
         _logger = logger;
     }
-
+    
+    /// <summary>
+    /// Get province name in Vietnam by GHN service
+    /// </summary>
+    /// <returns>provinceId and provinceName</returns>
     [HttpGet("provinces")]
     public async Task<IActionResult> GetProvince()
     {
@@ -27,7 +31,11 @@ public class ShippingController : ControllerBase
         var response = await _shippingService.GetProvinceAsync();
         return ResponseExtension.Result(response);
     }
-
+    /// <summary>
+    /// Get all district name by provinceId
+    /// </summary>
+    /// <param name="provinceId">Pass provinceId from GHN service</param>
+    /// <returns>districtId and districtName</returns>
     [HttpGet("districts/{provinceId}")]
     public async Task<IActionResult> GetDistrict(int provinceId)
     {
@@ -35,7 +43,11 @@ public class ShippingController : ControllerBase
         var response = await _shippingService.GetDistrictAsync(provinceId);
         return ResponseExtension.Result(response);
     }
-
+    /// <summary>
+    /// Get all ward name by districtId
+    /// </summary>
+    /// <param name="districtId">Pass districtId in GHN service</param>
+    /// <returns>return wardId and wardName</returns>
     [HttpGet("wards/{districtId}")]
     public async Task<IActionResult> GetWard(int districtId)
     {
@@ -43,12 +55,11 @@ public class ShippingController : ControllerBase
         var response = await _shippingService.GetWardAsync(districtId);
         return ResponseExtension.Result(response);
     }
-
-
+    
     /// <summary>
-    ///     Get shipping fee of order
+    /// Get shipping fee of order by passing FromDistrictId, FromWardCode, TotalWeight (int)
     /// </summary>
-    /// <param name="model"></param>
+    /// <param name="model">FromDistrictId, FromWardCode, TotalWeight (int)</param>
     /// <returns></returns>
     [HttpGet("fee")]
     public async Task<IActionResult> GetShippingFee([FromQuery] ShippingFeeRequestModel model)
@@ -59,9 +70,9 @@ public class ShippingController : ControllerBase
     }
 
     /// <summary>
-    ///     Create order shipping in ghn
+    /// Create order shipping in ghn
     /// </summary>
-    /// <param name="orderId"></param>
+    /// <param name="orderId">orderId in Yumilk shop</param>
     /// <returns></returns>
     [HttpPost("order/create/{orderId}")]
     [Authorize(AuthenticationSchemes = "Access", Roles = "1,2")]
@@ -73,9 +84,9 @@ public class ShippingController : ControllerBase
     }
 
     /// <summary>
-    ///     Preview order shipping in ghn
+    ///  Preview order shipping in ghn this will only preview not create order in GHN service
     /// </summary>
-    /// <param name="orderId"></param>
+    /// <param name="orderId">orderId in Yumilk shop</param>
     /// <returns></returns>
     [HttpPost("order/preview/{orderId}")]
     public async Task<IActionResult> PreviewOrder(Guid orderId)
@@ -86,10 +97,9 @@ public class ShippingController : ControllerBase
     }
 
     /// <summary>
-    ///     Get tracking order ghn by our Guid orderId (not ghn shippingCode)
-    ///     Get tracking log from GHN service
+    /// Get tracking log from GHN service
     /// </summary>
-    /// <param name="orderId"></param>
+    /// <param name="orderId">orderId in Yumilk Shop</param>
     /// <returns></returns>
     [HttpGet("order/tracking/{orderId}")]
     [Authorize(AuthenticationSchemes = "Access")]
@@ -101,9 +111,9 @@ public class ShippingController : ControllerBase
     }
 
     /// <summary>
-    ///     Cancel shipping order by our Guid orderId (not ghn shippingCode)
+    ///  Cancel shipping order by GHN service
     /// </summary>
-    /// <param name="orderId"></param>
+    /// <param name="orderId">orderId in Yumilk Shop</param>
     /// <returns></returns>
     [HttpPost("order/cancel/{orderId}")]
     [Authorize(AuthenticationSchemes = "Access", Roles = "1,2")]
